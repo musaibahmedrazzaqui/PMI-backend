e327024187f164cc8f0d8007c3cca92d69d8633e
musaibahmedrazzaqui <104699056+musaibahmedrazzaqui@users.noreply.github.com>
2023-01-16 17:58:00 +0500

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createPool({
   connectionLimit: 100,
-  host: "localhost",
-  user: "root",
-  password: "iloveallah",
-  database: "fyp",
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6591017",
+  password: "5kCiR8D8LL",
+  database: "sql6591017",
   port: 3306,
 });
 
