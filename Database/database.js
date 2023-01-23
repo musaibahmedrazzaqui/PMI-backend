5ac9eb9786118104e7a61eba3b648f13c2cc60c3
musaibahmedrazzaqui <104699056+musaibahmedrazzaqui@users.noreply.github.com>
2023-01-23 16:22:19 +0500

    s

--- Database/database.js
+++ Database/database.js
@@ -1,11 +1,19 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createPool({
+//   connectionLimit: 100,
+//   host: "sql6.freemysqlhosting.net",
+//   user: "sql6591017",
+//   password: "5kCiR8D8LL",
+//   database: "sql6591017",
+//   port: 3306,
+// });
 var connection = mysql.createPool({
   connectionLimit: 100,
-  host: "sql6.freemysqlhosting.net",
-  user: "sql6591017",
-  password: "5kCiR8D8LL",
-  database: "sql6591017",
+  host: "localhost",
+  user: "id20086066_pmi",
+  password: "",
+  database: "id20086066_fyp",
   port: 3306,
 });
 
